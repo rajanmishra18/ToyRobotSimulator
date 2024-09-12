63051d28fc854568fe109d1d886e31a31832eec0
dashanan <rajamishra18@gmail.com>
2024-09-12 11:53:45 +0530

    structure modified for production

---
+++ ToyRobotSimulator/Entities/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyRobotSimulator.Entities
+{
+    internal class Class1
+    {
+        // Class Related to DB connection or ENtities
+    }
+}
